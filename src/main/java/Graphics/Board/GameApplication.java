859294749901df3e2df17e64c39a37f683c80eee
l0renor <leon.lukas11@web.de>
2019-05-07 13:09:23 +0200

    grahics work more

--- src/main/java/Graphics/Board/GameApplication.java
+++ src/main/java/Graphics/Board/GameApplication.java
@@ -1,5 +1,6 @@
 package Graphics.Board;
 
+import Graphics.accessories.Accessorie;
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
@@ -10,6 +11,8 @@ import javafx.scene.paint.Color;
 import javafx.scene.shape.ArcType;
 import javafx.stage.Stage;
 
+import java.util.List;
+
 public class GameApplication extends Application {
 
     public static void main(String[] args) {
@@ -19,19 +22,37 @@ public class GameApplication extends Application {
     @Override
     public void start(Stage primaryStage) {
         //Configuration all done here or loaded
+        //start set up board
         BoardData boardData = new BoardData(500.0,500.0,2,"foo","TestGame");
+        boardData.addAccessorie(new Accessorie(100, 100, 230, 230, 1, "file:src/resources/scorp2.jpg") {
+            @Override
+            public void onClick() {
+
+            }
+        });
+        boardData.addAccessorie(new Accessorie(100, 100, 80, 80, 1, "file:src/resources/scorpion.png") {
+            @Override
+            public void onClick() {
+
+            }
+        });
+        //end set up board
         primaryStage.setTitle(boardData.getGameName());
         Group root = new Group();
         javafx.scene.canvas.Canvas canvas = new javafx.scene.canvas.Canvas(boardData.getSizeX(), boardData.getSizeY());
         GraphicsContext gc = canvas.getGraphicsContext2D();
-        drawShapes(gc);
+        drawShapes(gc,boardData);
         root.getChildren().add(canvas);
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
 
-    private void drawShapes(GraphicsContext gc) {
-        Image testimg = new Image("file:src/resources/scorp2.jpg");
-        gc.drawImage(testimg,250, 250,60,60);
+    private void drawShapes(GraphicsContext gc,BoardData boardData) {
+        List<Accessorie> accessories = boardData.getAccessories();
+        for(Accessorie acc : accessories){
+            Image testimg = new Image(acc.getPathToBackgground());
+            gc.drawImage(testimg,acc.getPosX(), acc.getPosY(),acc.getSizeX(),acc.getSizeY());
+        }
+
     }
 }
