01dd24a1c2a7d1ca0864b830af5e196baa9d7044
kakomarina <ninakako@hotmail.com>
2019-05-09 11:31:18 -0300

    Changes in accessory and accessories added.

---
+++ src/main/java/framework/data/Accessory.java
@@ -0,0 +1,62 @@
+package framework.data;
+
+
+public class Accessory implements Comparable<Accessory> {
+
+    private int sizeX;
+    private int sizeY;
+    private int posX;
+    private int posY;
+
+    private int layer;
+    private String pathToImage;
+
+    public Accessory(int sizeX, int sizeY, int posX, int posY, int layer, String pathToImage) {
+        this.sizeX = sizeX;
+        this.sizeY = sizeY;
+        this.posX = posX;
+        this.posY = posY;
+        this.layer = layer;
+        this.pathToImage = pathToImage;
+    }
+
+    public int getSizeX() {
+        return sizeX;
+    }
+
+    public int getSizeY() {
+        return sizeY;
+    }
+
+    public int getPosX() {
+        return posX;
+    }
+
+    public int getPosY() {
+        return posY;
+    }
+
+    public String getPathToImage() {
+        return pathToImage;
+    }
+
+    public void setPathToImage(String newPath){
+        this.pathToImage = newPath;
+    }
+
+    public int getLayer() {
+        return layer;
+    }
+
+    /**
+     * Compare based on layer; high to low
+     * @param other other Accessory to be compared to
+     * @return int to indicate relation between the objects
+     */
+
+    public int compareTo(Accessory other) {
+        return this.layer - other.layer;
+    }
+
+
+}
---
+++ src/main/java/framework/data/Dice.java
@@ -0,0 +1,31 @@
+package framework.data;
+
+import java.util.Random;
+
+public class Dice extends Accessory {
+
+    int numSides;
+    String[] pathToSides;
+
+    public Dice(int sizeX, int sizeY, int posX, int posY, int layer, String[] pathToSides) {
+        super(sizeX, sizeY, posX, posY, layer, pathToSides[0]);
+        this.numSides = pathToSides.length;
+    }
+
+    /**
+     * It will roll the dice and change the image according to the side that should be facing up
+     */
+    public void onClick() {
+        int side = this.roll();
+        this.setPathToImage(pathToSides[side]);
+    }
+
+    /**
+     * Simulates rolling the dice
+     * @return random generated number
+     */
+    public int roll(){
+        return (new Random().nextInt(numSides)) + 1;
+    }
+
+}
