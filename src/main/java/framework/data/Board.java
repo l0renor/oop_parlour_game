fcfdaef6424bdf15b7f6a216cfe288557b04feb0
Fabian Reinold <32450519+freinold@users.noreply.github.com>
2019-05-13 15:42:32 +0200

    Revert "Sprint 2 marina"

--- src/main/java/framework/data/Board.java
+++ src/main/java/framework/data/Board.java
@@ -5,18 +5,75 @@ import java.util.List;
 
 public class Board {
 
-    private double sizeX;
-    private double sizeY;
 
-    private int numLayers; //layer 0 is background
-    private String pathToBackground;
-    private String gameName;
-    private ArrayList<ArrayList<Accessory>> accessoriesByLayer; // sorted by layer?
     private double width;
     private double height;
     private String infoText;
-  
-      /**
+
+    /**
+     * the number of layers on the game board; excluding the background
+     */
+    private int numLayers; //layer 0 is background
+    /**
+     * path to the backgroundimage e.g. "file:src/resources/board.png"
+     */
+    private String pathToBackgground;
+    /**
+     * The name of the game; displayed above the canvas
+     */
+    private String gameName;
+
+    private ArrayList<ArrayList<Accessory>> accessoriesByLayer;
+
+    /**
+     * Getter for width of the gameboard/canvas.
+     * @return width of the gameboard/canvas.
+     */
+    public double getWidth() {
+        return width;
+    }
+
+    /**
+     * Getter for height of the gameboard/canvas.
+     * @return height of the gameboard/canvas.
+     */
+    public double getHeight() {
+        return height;
+    }
+
+    public String getPathToBackgground() {
+        return pathToBackgground;
+    }
+
+    public List<ArrayList<Accessory>> getAccessoriesByLayer() {
+        return accessoriesByLayer;
+    }
+
+    public String getGameName() {
+        return gameName;
+    }
+
+    public void setGameName(String gameName) {
+        this.gameName = gameName;
+    }
+
+    /**
+     * Getter for the infotext displayed below the gameboard; e.g. for instructions
+     * @return the infotext
+     */
+    public String getInfoText() {
+        return infoText;
+    }
+
+    /**
+     * Setter for infotext displayed below the gameboard; e.g. for instructions
+     * @param infoText the new info text; the old one is deleted
+     */
+    public void setInfoText(String infoText) {
+        this.infoText = infoText;
+    }
+
+    /**
      * Constructor of a game bord.
      * @param width of the board
      * @param height of the board
@@ -37,65 +94,12 @@ public class Board {
         this.infoText = "";
     }
 
-
-    public double getSizeX() {
-        return sizeX;
-    }
-
-    public double getSizeY() {
-        return sizeY;
-    }
-
-    public String getPathToBackground() {
-        return pathToBackground;
-    }
-
-    /**
-     * Getter for width of the gameboard/canvas.
-     * @return width of the gameboard/canvas.
-     */
-    public double getWidth() {
-        return width;
-    }
-
-    /**
-     * Getter for height of the gameboard/canvas.
-     * @return height of the gameboard/canvas.
-     */
-    public double getHeight() {
-        return height;
-    }
-
-    public List<ArrayList<Accessory>> getAccessoriesByLayer() {
-        return accessoriesByLayer;
-    }
-
-    public String getGameName() {
-        return gameName;
-    }
-
-    public void setGameName(String gameName) {
-        this.gameName = gameName;
-    }
-
-
-
-    /**
-     * Setter for infotext displayed below the gameboard; e.g. for instructions
-     * @param infoText the new info text; the old one is deleted
-     */
-    public void setInfoText(String infoText) {
-        this.infoText = infoText;
-    }
-
-
-
     /**
      * Adds an accessorie to the gameboard.     *
      * @param a accessorie to add
      * @return boolean  if sucsess
      */
-    public boolean addAccessory(Accessory a) {
+    public boolean addAccessorie(Accessory a) {
         //@TODO check overlapp on one layer -> is not allowed
         if (a.getLayer() > numLayers) {
             return false; //@TODO exception?
@@ -105,8 +109,6 @@ public class Board {
         return accessoriesByLayer.get(a.getLayer()).add(a); //add accessorie to the right layer
     }
 
-
-          
     /**
      * Number of graphical layers on the canvas.
      * The background is excluded
@@ -116,9 +118,5 @@ public class Board {
         return numLayers;
     }
 
-    public void setNumLayers(int numLayers) {
-        this.numLayers = numLayers;
-    }
- 
 }
 
