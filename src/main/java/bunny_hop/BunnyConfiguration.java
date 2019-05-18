81b2aac27254c62937e13ea71f55848a2662c1d6
l0renor <leon.lukas11@web.de>
2019-05-18 16:49:36 +0200

    start screen works

--- src/main/java/bunny_hop/BunnyConfiguration.java
+++ src/main/java/bunny_hop/BunnyConfiguration.java
@@ -11,7 +11,6 @@ import java.util.ArrayList;
 
 public class BunnyConfiguration extends Configuration {
     public void configure() {
-        //setStartBoard( new BunnyLayout().createLayout());
         ArrayList<GameMode> gameModes = new ArrayList<>();
         gameModes.add(new GameMode() {
             @Override
